3076586033ec2ecd851ec9e61fcb5784188af707
sunshinesong <7743674@qq.com>
2023-02-07 15:01:34 +0800

    init array extension with List

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # array
 用于flutter使用的数组拓展方法 (List extension function)
+
+之后会拓展出更多的方法！
+
+## 添加依赖
+```javascript
+  dependencies:
+    array: ^0.0.1
+```
+
+## 一个简单的示例 (flat)
+```javascript
+  import 'package:array/array.dart';
+
+  // 数组扁平化
+  List list = [1, [2, [3, { 'name': [123] }]]];
+  print(list.flat(1)); // [1, 2, [3, {name: [123]}]]
+  print(list.flat()); // 默认是展开1层， [1, 2, [3, {name: [123]}]]
+
+  // 如果你不知道数组的深度，你可以传入double.infinity：
+  print(list.flat(double.infinity)); // [1, 2, 3, {name: [123]}]
+
+```
+
+## 一个简单的示例 (flatMap)
+```javascript
+  import 'package:array/array.dart';
+
+  // 数组扁平化
+  // 它和flat不一样的是，它只会展开一层，而且接收一个方法，用于映射出对应的值
+  List list = [{ 'name': 123, 'age': 0 }, { 'name': 124, 'age': 1 }];
+  List res = list.flatMap((e) {
+   print(e);
+   return e['name'];
+  });
+  
+  print(res);  // [123, 124]
+
+```
+
+## 一个简单的示例 (filter)
+```javascript
+  import 'package:array/array.dart';
+
+  // 数组过滤，和js一样
+  List list = [{ 'name': 123, 'age': 0 }, { 'name': 124, 'age': 1 }];
+  List res = list.filter((item, index, list) {
+   return item['name'];
+  });
+  
+  print(res);  // [{name: 123, age: 0}]
+
+```
---
+++ lib/array.dart
@@ -0,0 +1,93 @@
+library array;
+
+extension L on List {
+
+  /// 
+  /// Returns a new list
+  /// 
+  /// ```dart
+  /// List list = [1, [2, [3, { 'name': [123] }]]];
+  /// print(list.flat(1)); // [1, 2, [3, {name: [123]}]]
+  /// ```
+  ///
+  /// If [depth] is null, it defaults to the [1] of this list.
+  ///
+  /// ```dart
+  /// List list = [1, [2, [3, { 'name': [123] }]]];
+  /// print(list.flat()); // [1, 2, [3, {name: [123]}]]
+  /// ```
+  ///
+  /// If you confusion this list length, you fully can set as [double.infinity]
+  /// List list = [1, [2, [3, { 'name': [123] }]]];
+  /// print(list.flat(double.infinity)); // [1, 2, 3, {name: [123]}]
+  List flat([double? depth = 1]) {
+    List result = [];
+
+    flatDeep(arr, depth) {
+      arr.forEach((val) {
+        if (depth > 0 && val is List) {
+          flatDeep(val, depth - 1);
+        } else {
+          result.add(val);
+        }
+      });
+    }
+    if (depth != null && depth < 0 || isEmpty) return this;
+
+    flatDeep(this, depth ?? 1);
+    return result;
+  }
+
+  /// 
+  /// Returns a new list
+  /// 
+  /// It's different from flat, it just flat[depth] for 1.
+  /// 
+  /// ```dart
+  /// List list = [{ 'name': 123, 'age': 0 }, { 'name': 124, 'age': 1 }];
+  /// List res = list.flatMap((e) {
+  ///  print(e);
+  ///  return e['name'];
+  /// });
+  /// 
+  /// print(res);  // [123, 124]
+  /// ```
+  List flatMap(fn) {
+    return map(fn).toList().flat(1);
+  }
+
+  /// 
+  /// Returns a new list
+  /// 
+  /// ```dart
+  /// List list = [{ 'name': 123, 'age': 0 }, { 'name': 124, 'age': 1 }];
+  /// List res = list.filter((item, index, list) {
+  ///  print(item); // it's current item
+  ///  print(index); // it's current index
+  ///  print(list); // it's source list
+  ///  return item['name'];
+  /// });
+  /// 
+  /// print(res);  // [{name: 123, age: 0}]
+  /// ```
+  List filter(Function fun) {
+    if (isEmpty) return this;
+
+    List t = this;
+    int len = t.length >>> 0;
+
+    List res = [];
+    for (var i = 0; i < len; i++) {
+      var val = t[i];
+      try {
+        if (fun(val, i, t)) {
+          res.add(val);
+        }
+      } catch (e) {
+        throw ArgumentError.value('current introduced Function not return a bool type');
+      }
+    }
+
+    return res;
+  }
+}
